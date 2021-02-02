6a6d55740a522ac4396371f0a200e814b21e2dab
yangguang@forchange <yangguang@forchange.tech>
2021-02-02 22:39:13 +0800

    angular-app -> angular-hero

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-app app is running!');
+    expect(await page.getTitleText()).toEqual('angular-hero app is running!');
   });
 
   afterEach(async () => {
